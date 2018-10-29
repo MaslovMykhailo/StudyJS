d906a87b17713e204189822271ee44e0789c71e0
maslovmichail20 <maslov.mikhail2000@gmail.com>
2018-10-29 21:55:32 +0200

    Create array isSorted function

---
+++ arrays/isSorted.js
@@ -0,0 +1,9 @@
+const isSorted = arr => {
+  if (!arr.length) return true;
+  
+  return arr.join() === arr.sort((a, b) => a - b).join();
+};
+
+console.log(isSorted([]));
+console.log(isSorted([-Infinity, -5, 0, 3, 9]));
+console.log(isSorted([3, 9, -3, 10]));
